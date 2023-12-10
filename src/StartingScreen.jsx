55e434470a5eeda7e9b817168a18bc24c8815e8a
Kat L <kathyl123@gmail.com>
2023-12-10 02:31:23 -0800

    first commit, added starting screen

---
+++ src/StartingScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function StartingScreen() {
+
+    
+    return (
+        <div className="startscreen">
+            <h1 className="title">That's what <span className="strikeout">she</span> they said!</h1>
+            <button className="start-quiz">start quiz</button>
+            {/* <img className="startscreen-background" src={background} alt="ripped paper border with a grid background and the office banner"></img> */}
+            
+        </div>
+    )
+}
